29f6d9ec29d5aa2e2b37eaa040d434df42905a31
ValerySuarez22 <valerysuarez2296@gmail.com>
2023-04-03 15:13:23 +0200

    cambios

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import "../navbar/navbar.css";
-
-// function Navbar() {
-//   return (
-//     <nav className="navbar">
-//       <div className="navbar-logo">
-//         <img className="logo" src="./assets/images/logo.png" alt="logo" />
-//       </div>
-//       <a href="#login" className="navbar-link">
-//         Home
-//       </a>
-//       <a href="#" className="navbar-link">
-//         About
-//       </a>
-//       <a href="#" className="navbar-link">
-//         Services
-//       </a>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 import React from "react";
 import Logo from "../../assets/images/logo.png"
 import "../navbar/navbar.css";
