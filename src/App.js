918f6f3c50923484ea48b27deae394f1d8ca9d88
CodeMincer <codemincer@gmail.com>
2023-04-04 09:42:33 +0200

    add calendar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Login from './views/login/Login';
 import HighForm from './components/highForm/HighForm';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './App.css';
 
+const localizer = momentLocalizer(moment);
+
 function App() {
+  const [events, setEvents] = useState([
+    {
+      start: new Date(),
+      end: new Date(),
+      title: 'Ejemplo de evento',
+    },
+  ]);
+
   return (
     <div>
       <header >
        {/* <Navbar /> */}
        <Login />
        <HighForm />
+       <Calendar localizer={localizer} events={events} />
       </header>
     </div>
   );
---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import '../calendar/calendar.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = props => (
+  <div className="calendar-container">
+    <Calendar
+      localizer={localizer}
+      events={props.events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+      className='calendar-color'
+    />
+  </div>
+);
+
+export default MyCalendar;
