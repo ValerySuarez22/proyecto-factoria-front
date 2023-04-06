521c12c049df415d784c262172d033f8d3b0c96f
Auri <aurigutierrez499@gmail.com>
2023-04-06 15:43:50 +0200

    actualizacion de las cards

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Navbar from './components/navbar/Navbar';
 import Login from './views/login/Login';
 import './App.css';
 import Cards from './components/cards/Cards';
-
+import CardsEvalue from './components/cards-evalue/CardsEvalue';
+import CardsSedes from './components/cards-sedes/CardsSedes';
 function App() {
   return (
     <div>
       <header >
-       {/* {<Navbar /> */}
-       {/* {<Login /> */}
+       <Navbar />
+       <Login /> 
        <Cards />
+       <CardsEvalue /> 
+       <CardsSedes />
       </header>
     </div>
   );
