782fddc8580fce90348aca78bd92176252cad665
CodeMincer <codemincer@gmail.com>
2023-04-08 18:56:24 +0200

    cambios app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Login from './views/login/Login';
 import HighForm from './components/highForm/HighForm';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './App.css';
 
-const localizer = momentLocalizer(moment);
-
-function App() {
-  const [events, setEvents] = useState([
-    {
-      start: new Date(),
-      end: new Date(),
-      title: 'Ejemplo de evento',
-    },
-  ]);
 
   return (
     <div>
@@ -24,7 +11,6 @@ function App() {
        {/* <Navbar /> */}
        <Login />
        <HighForm />
-       <Calendar localizer={localizer} events={events} />
       </header>
     </div>
   );
