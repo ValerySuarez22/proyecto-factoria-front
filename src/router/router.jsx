8ab0591c9e35a6b6f93d36cc510e79affb79a67a
ValerySuarez22 <valerysuarez2296@gmail.com>
2023-04-19 09:36:31 +0200

    .

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-// import React from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "../../src/views/humanResources/homeRRHH/Home"
-// import Register from "../../src/views/humanResources/register/Register"
-// import TeamsArea from "../views/humanResources/teams/TeamsArea";
-// import ListWorkers from "../views/humanResources/workersPP/ListWorkers";
-
-
-// const Router = () => {
-//    return (
-//       <BrowserRouter>
-//          <Routes>
-//             <Route path="/" element={<Home/>} />
-//             <Route path="/register" element={<Register/>} />
-//             <Route path="/TeamsArea" element={<TeamsArea/>} />
-//             <Route path="/listWorkers" element={<ListWorkers/>} />
-//          </Routes>
-//       </BrowserRouter>
-//    );
-// };
-
-// export default Router
-=======
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../../src/views/humanResources/homeRRHH/Home";
@@ -57,4 +33,3 @@ const Router = () => {
 };
 
 export default Router;
->>>>>>> origin/unificar
