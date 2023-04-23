d5b894087b83b947c1bc8a5bbd60e8e4070c5e36
CodeMincer <codemincer@gmail.com>
2023-04-23 19:23:48 +0200

    setLoading

--- src/views/humanResources/workersPP/ListWorkers.jsx
+++ src/views/humanResources/workersPP/ListWorkers.jsx
@@ -6,19 +6,31 @@ import customActions from '../../../components/actions';
 
 const ListWorkers = () => {
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     customActions()
       .then(result => {
         console.log('result', result)
         setUser(result)
+        setIsLoading(false);
       })
   }, [])
 
   return (
     <div className='containerList'>
       <Navbar user={user} />
+      {isLoading ? (
+        <div className='loading-list-workers'>
+          <p className='loading-cards'>Un momento, por favor.<br /> Los datos se están cargando...</p>
+          <div className="spinner">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
+          </div>
+        </div>
+      ) : (
       <Cards user={user} />
+      )}
     </div>
   )
 }
